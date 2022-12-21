cd4873c55676c2309284a26f59ce980dcaa562fb
Mahmut Karatay <mahmutkaratay61@gmail.com>
2022-12-21 11:18:52 +0300

    tasarım güncellemesi

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import "./App.css";
-import City from './City'
+import City from "./City";
 
 function App() {
   const key = "86faff0478c079b71968ced721197a73";
@@ -11,7 +11,6 @@ function App() {
   useEffect(() => {
     async function getApi() {
       try {
-        
         const response = await axios.get(
           `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=${key}&units=metric`
         );
@@ -25,17 +24,33 @@ function App() {
     getApi();
   }, [search]);
 
-   
-
   return (
     <div className="api">
-        <input
-          onChange={(e) => setSearch(e.target.value)}
-          className="text border-gray-300"
-          type="px-3 border-3"
-        />
-        <City city={selectedCity}></City>
-      
+      <div class="overflow-hidden shadow sm:rounded-md">
+        <div class="bg-white px-4 py-5 sm:p-6">
+          <div class="grid grid-cols-6 gap-6">
+            <div class="col-span-6 sm:col-span-3">
+              <label
+                for="first-name"
+                class="block text-sm font-medium text-gray-700"
+              >
+                Şehir Ara:
+              </label>
+              <input
+                onChange={(e) => setSearch(e.target.value)}
+                type="text"
+                name="first-name"
+                id="first-name"
+                autocomplete="given-name"
+                class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+              />
+            </div>
+            <City city={selectedCity}></City>
+          </div>
+        </div>
+      </div>
+
+       
     </div>
   );
 }
